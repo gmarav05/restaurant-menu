80550a7e285d7becc9991cbbbfdde24b4d8e093e
Aravind <gmarav005@gmail.com>
2025-11-02 11:33:11 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,23 @@
+const menuArray = [
+    {
+        name: "Idly",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕"
+    },
+    {
+        name: "Dosa",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1
+    },
+        {
+        name: "Meals",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        emoji: "🍺",
+        id: 2
+    }
+]
